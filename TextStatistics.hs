947d45245b8cc41be077ba3153beb46bbabe8c6e
Maarten van Gompel <proycon@anaproy.nl>
2014-11-23 20:47:58 +0100

    elegance

--- TextStatistics.hs
+++ TextStatistics.hs
@@ -1,26 +1,36 @@
-module TextStatistics (getcount,count,count1,makefreqlist,ngramfreqlist) where
+module TextStatistics (count,addcount,addcount1,totalcount,freqlist,ngramfreqlist) where
 import TextProcessors
 import qualified Data.HashMap.Lazy as HashMap
 import Data.Hashable
 import Data.Maybe
 
 
-getcount :: (Eq a, Hashable a) => a -> HashMap.HashMap a Int -> Int
-getcount ngram freqlist = if value == Nothing then 0 else fromJust value
-                     where value = HashMap.lookup ngram freqlist
+count :: (Eq a, Hashable a) => a -> HashMap.HashMap a Int -> Int
+count ngram freqlist = case value of Nothing -> 0
+                                     Just x  -> x
+                       where value = HashMap.lookup ngram freqlist
 
-count :: (Eq a, Hashable a) => a -> Int -> HashMap.HashMap a Int -> HashMap.HashMap a Int
-count ngram c freqlist= HashMap.insert ngram ((getcount ngram freqlist) + c) freqlist
+freq :: (Eq a, Hashable a) => a -> HashMap.HashMap a Int -> Double
+freq ngram freqlist = case value of Nothing -> 0.0
+                                    Just x  -> (fromIntegral x) / (fromIntegral x)
+                      where value = HashMap.lookup ngram freqlist
 
-count1 :: (Eq a, Hashable a) => a -> HashMap.HashMap a Int -> HashMap.HashMap a Int
-count1 ngram freqlist = count ngram 1 freqlist
+totalcount :: HashMap.HashMap a Int -> Int
+totalcount freqlist = sum (HashMap.elems freqlist)
 
-makefreqlist :: (Eq a, Hashable a) => [a] -> HashMap.HashMap a Int 
-makefreqlist []         = HashMap.empty
-makefreqlist (ngram:xs) = count1 ngram (makefreqlist xs) 
+addcount :: (Eq a, Hashable a) => a -> Int -> HashMap.HashMap a Int -> HashMap.HashMap a Int
+addcount ngram c freqlist= HashMap.insert ngram ((count ngram freqlist) + c) freqlist
+
+addcount1 :: (Eq a, Hashable a) => a -> HashMap.HashMap a Int -> HashMap.HashMap a Int
+addcount1 ngram freqlist = addcount ngram 1 freqlist
+
+
+freqlist :: (Eq a, Hashable a) => [a] -> HashMap.HashMap a Int 
+freqlist []         = HashMap.empty
+freqlist (ngram:xs) = addcount1 ngram (freqlist xs) 
 
 ngramfreqlist :: Int -> [String] -> HashMap.HashMap [String] Int
-ngramfreqlist n words = makefreqlist (ngrams n words)
+ngramfreqlist n words = freqlist (ngrams n words)
 
 
 
