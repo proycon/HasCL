38766e5c3dfca370e548debc6aace69e92df84b6
Maarten van Gompel <proycon@anaproy.nl>
2014-11-23 21:37:19 +0100

    update

--- TextStatistics.hs
+++ TextStatistics.hs
@@ -12,8 +12,9 @@ count ngram freqlist = case value of Nothing -> 0
 
 freq :: (Eq a, Hashable a) => a -> HashMap.HashMap a Int -> Double
 freq ngram freqlist = case value of Nothing -> 0.0
-                                    Just x  -> (fromIntegral x) / (fromIntegral x)
+                                    Just x  -> (fromIntegral x) / (fromIntegral total)
                       where value = HashMap.lookup ngram freqlist
+                            total = totalcount freqlist
 
 totalcount :: HashMap.HashMap a Int -> Int
 totalcount freqlist = sum (HashMap.elems freqlist)
