1be1afcab82a2dde0dedc6026236e8dd19c2bd65
Maarten van Gompel (proycon) <proycon@anaproy.nl>
2015-05-17 23:31:14 +0200

    update

--- TextStatistics.hs
+++ TextStatistics.hs
@@ -22,13 +22,9 @@ totalcount freqlist = sum (HashMap.elems freqlist)
 addcount :: (Eq a, Hashable a) => a -> Int -> HashMap.HashMap a Int -> HashMap.HashMap a Int
 addcount ngram c freqlist= HashMap.insert ngram ((count ngram freqlist) + c) freqlist
 
-addcount1 :: (Eq a, Hashable a) => a -> HashMap.HashMap a Int -> HashMap.HashMap a Int
-addcount1 ngram freqlist = addcount ngram 1 freqlist
-
-
 freqlist :: (Eq a, Hashable a) => [a] -> HashMap.HashMap a Int 
 freqlist []         = HashMap.empty
-freqlist (ngram:xs) = addcount1 ngram (freqlist xs) 
+freqlist (ngram:xs) = addcount ngram 1 (freqlist xs) 
 
 ngramfreqlist :: Int -> [String] -> HashMap.HashMap [String] Int
 ngramfreqlist n words = freqlist (ngrams n words)
