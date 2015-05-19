985bffb7e0607f80c4b41675eec2d808e7807afa
Maarten van Gompel <proycon@anaproy.nl>
2015-05-19 22:30:08 +0200

    comments

--- TextStatistics.hs
+++ TextStatistics.hs
@@ -11,18 +11,21 @@ count :: (Eq a, Hashable a) => HashMap.HashMap a Int -> a -> Int
 count freqlist token  = case value of Nothing -> 0
                                       Just x  -> x
                         where value = HashMap.lookup token freqlist
---
--- relative frequency
+
+-- relative count (frequency)
 freq :: (Eq a, Hashable a) => HashMap.HashMap a Int -> a -> Double
 freq freqlist token = fromIntegral (count freqlist token) / fromIntegral (totalcount freqlist)
 
+-- total number of tokens
 totalcount :: HashMap.HashMap a Int -> Int
 totalcount freqlist = sum (HashMap.elems freqlist)
 
+-- frequency list
 freqlist :: (Eq a, Hashable a) => [a] -> HashMap.HashMap a Int 
 freqlist []         = HashMap.empty
 freqlist (token:xs) = HashMap.insert token ((count (freqlist xs) token) + 1) $ freqlist xs
 
+-- ngram frequency list
 ngramfreqlist :: Int -> [String] -> HashMap.HashMap [String] Int
 ngramfreqlist n words = freqlist (ngrams n words)
 
